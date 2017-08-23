a6b78e70715b81a3825ee464e29e3fb8e8620e78
Shaojung <shaojung@gmail.com>
2017-08-23 15:12:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc082301/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.yvtc.yvtc082301;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager manager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+    public void click1(View v)
+    {
+        Notification.Builder builder = new Notification.Builder(MainActivity.this);
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("這是提醒")
+                .setContentText("這是提醒的內容");
+        manager.notify(321, builder.build());
+    }
+}
