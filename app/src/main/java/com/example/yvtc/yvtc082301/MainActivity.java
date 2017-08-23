ca1af11710c67825e91d424c1b28450f84fc2913
Shaojung <shaojung@gmail.com>
2017-08-23 15:37:59 +0800

    onGoing

--- app/src/main/java/com/example/yvtc/yvtc082301/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc082301/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.yvtc.yvtc082301;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuAdapter;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
     }
     public void click1(View v)
     {
+        Intent it = new Intent(MainActivity.this, DetailActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 1212, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification.Builder builder = new Notification.Builder(MainActivity.this);
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("這是提醒")
                 .setContentText("這是提醒的內容")
+                .setContentIntent(pi)
                 .setOngoing(true);
+
         manager.notify(321, builder.build());
     }
     public void onCancel(View v)
